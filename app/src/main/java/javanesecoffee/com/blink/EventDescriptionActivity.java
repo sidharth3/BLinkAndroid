17399dcb34cfa3b4455f8a656b571ec93f2b1a1d
gaurisinghal <gauri.singhal171@gmail.com>
2019-10-30 16:25:55 +0800

    commit

---
+++ app/src/main/java/javanesecoffee/com/blink/EventDescriptionActivity.java
@@ -0,0 +1,16 @@
+package javanesecoffee.com.blink;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class EventDescriptionActivity extends AppCompatActivity {
+    //get the details of the event from the server and display them
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_description_activity);
+    }
+    
+
+}
