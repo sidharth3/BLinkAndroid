bea8640333ba32ac127f86cfa8a7fe7c635c2f27
gaurisinghal <gauri.singhal171@gmail.com>
2019-11-01 16:17:46 +0800

    user class

---
+++ app/src/main/java/javanesecoffee/com/blink/User.java
@@ -0,0 +1,8 @@
+package javanesecoffee.com.blink;
+
+public class User {
+    private String username = "username";
+    //what is the type of the profile picture
+    //private Array? picture = picture
+
+}
