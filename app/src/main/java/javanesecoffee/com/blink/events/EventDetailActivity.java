57fa3acc4ad682a459ce572326d2a18475c1f84a
Sean Lim <seanmichael_lim@mymail.sutd.edu.sg>
2019-11-26 07:18:27 +0800

    updated activity and detail page

--- app/src/main/java/javanesecoffee/com/blink/events/EventDetailActivity.java
+++ app/src/main/java/javanesecoffee/com/blink/events/EventDetailActivity.java
@@ -1,16 +1,23 @@
 package javanesecoffee.com.blink.events;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import javanesecoffee.com.blink.R;
 import javanesecoffee.com.blink.api.BLinkApiException;
 import javanesecoffee.com.blink.api.ImageLoadObserver;
+import javanesecoffee.com.blink.constants.IntentExtras;
 import javanesecoffee.com.blink.entities.Event;
+import javanesecoffee.com.blink.managers.EventManager;
 
 public class EventDetailActivity extends AppCompatActivity implements ImageLoadObserver {
     Event currentEvent;
@@ -18,16 +25,58 @@ public class EventDetailActivity extends AppCompatActivity implements ImageLoadO
     TextView eventName;
     TextView eventDate;
     TextView eventTime;
-    TextView eventAddress;
+    TextView eventLocation;
     TextView eventPrice;
     TextView eventDescription;
+
+
     Button eventRegister;
     RecyclerView eventTags;
     RecyclerView eventAlsoAttending;
 
+    Event EVENT_KEY;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_detail_page);
+
+        //textview
+        eventName = findViewById(R.id.event_detail_name);
+        eventDate = findViewById(R.id.event_detail_date);
+        eventTime = findViewById(R.id.event_detail_time);
+        eventLocation = findViewById(R.id.event_detail_location);
+        eventPrice = findViewById(R.id.event_detail_price);
+        eventDescription = findViewById(R.id.event_detail_description);
+
+
+        //button
+        eventRegister = findViewById(R.id.event_detail_register_button);
+
+        //recyclerview
+        eventTags = findViewById(R.id.event_detail_tags);
+        eventAlsoAttending = findViewById(R.id.event_also_attending_profile_pic);
+
+        Intent intent = getIntent();
+
+        UpdateData();
+
+    }
+
+    public void UpdateData(){
+        if(currentEvent!=null){
+            Log.d("EVENT DETIALS ACTIVITY", "UpdateData: EVENT DETAILS ACTIITY");
+            eventName.setText(currentEvent.getName());
+            eventDate.setText(currentEvent.getDate());
+            eventTime.setText(currentEvent.getTime());
+            eventLocation.setText(currentEvent.getAddress());
+            eventPrice.setText(currentEvent.getPrice());
+            eventDescription.setText(currentEvent.getDescription());
+        }
 
 
 
+    }
 
     @Override
     public void onImageLoad(Bitmap bitmap) {
