c1a2625cab7da8be95a2232332aa1eb13804597b
Elliot Koh <appdev.moose@gmail.com>
2019-11-26 11:34:37 +0800

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/javanesecoffee/com/blink/constants/Config.java
+++ app/src/main/java/javanesecoffee/com/blink/constants/Config.java
@@ -1,9 +1,9 @@
 package javanesecoffee.com.blink.constants;
 
 public class Config {
-    public static final BuildModes buildMode = BuildModes.BYPASS_ONBOARDING;
+    public static final BuildModes buildMode = BuildModes.PRODUCTION;
 //    public static final String DOMAIN = "http://192.168.1.88/";
-//    public static final String DOMAIN = "http://10.12.87.72/";
-    public static final String DOMAIN = "http://192.168.1.74/";
+    public static final String DOMAIN = "http://10.12.87.72/";
+//    public static final String DOMAIN = "http://192.168.1.74/";
 }
 
